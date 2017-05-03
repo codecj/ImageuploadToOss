cdf1f2e57c6103b5df4fba0441221accda965949
liudunhui <liudunhui@11wlw.com>
2017-05-03 17:27:47 +0800

    空行

--- src/main.js
+++ src/main.js
@@ -17,46 +17,43 @@ Vue.use(VueRouter);
 // 定义组件, 也可以像教程之前教的方法从别的文件引入
 // const First = { template: '<div><h2>我是第 1 个子页面</h2></div>' }
 import secondcomponent from './component/secondcomponent.vue'
-import  First from './component/firstcomponent.vue'
+import First from './component/firstcomponent.vue'
 
 // 创建一个路由器实例
 // 并且配置路由规则
 const router = new VueRouter({
-  mode: 'history',
-  base: __dirname,
-  routes: [
-    {
-      path: '/',
-      component: First
-    },
-    {
-      path: '/second',
-      component: secondcomponent
-    }
-  ]
+    mode: 'history',
+    base: __dirname,
+    routes: [{
+        path: '/',
+        component: First
+    }, {
+        path: '/second',
+        component: secondcomponent
+    }]
 })
 
 
 router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    // this route requires auth, check if logged in
-    // if not, redirect to login page.
-    if (!auth.loggedIn()) {
-      next({
-        path: '/login',
-        query: { redirect: to.fullPath }
-      })
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        // this route requires auth, check if logged in
+        // if not, redirect to login page.
+        if (!auth.loggedIn()) {
+            next({
+                path: '/login',
+                query: { redirect: to.fullPath }
+            })
+        } else {
+            next()
+        }
     } else {
-      next()
+        next() // 确保一定要调用 next()
     }
-  } else {
-    next() // 确保一定要调用 next()
-  }
 })
 
 // 现在我们可以启动应用了！
 // 路由器会创建一个 App 实例，并且挂载到选择符 #app 匹配的元素上。
 const app = new Vue({
-  router: router,
-  render: h => h(App)
+    router: router,
+    render: h => h(App)
 }).$mount('#app')
