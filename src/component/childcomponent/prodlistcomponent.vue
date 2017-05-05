3ca7091c2364a33083bddf72445bfccc6c543fde
cuijing <cuijing@11wlw.com>
2017-05-05 14:56:03 +0800

    360list

---
+++ src/component/childcomponent/prodlistcomponent.vue
@@ -0,0 +1,311 @@
+<template>
+  <div id="prodsList">
+     <!-- content横着布局和changeItem竖着布局-->
+     <div class="proList">
+        <div :class="{'changeItem':listStatus,'content':!listStatus}">
+      <!-- 每个产品 -->
+         <div class="searchItem">
+          <div class="onephoto">
+            <img src="../../assets/icon12.png" alt="" class="photo">
+          </div>
+          
+          <p>墨西哥辣鸡点心啊啊啊啊啊啊啊啊啊啊啊</p>
+          <p>规格：4444*777</p>
+          <p>
+              <span>优惠套餐</span>
+              <span>混搭满赠</span>
+              <span>打折</span>
+            </p>
+          <p>
+            <span>￥20.00</span>
+            <img src="../../assets/icon43.png" alt="" class="gocart">
+          </p>
+        </div>
+        <div class="searchItem">
+          <div class="onephoto">
+            <img src="../../assets/icon12.png" alt="" class="photo">
+          </div>
+          
+          <p>墨西哥辣鸡点心啊啊啊啊啊啊啊啊啊啊啊</p>
+          <p>规格：4444*777</p>
+          <p>
+              <span>优惠套餐</span>
+              <span>混搭满赠</span>
+              <span>打折</span>
+            </p>
+          <p>
+            <span>￥20.00</span>
+            <img src="../../assets/icon43.png" alt="" class="gocart">
+          </p>
+        </div>
+         <div class="searchItem">
+          <div class="onephoto">
+            <img src="../../assets/icon12.png" alt="" class="photo">
+          </div>
+          
+          <p>墨西哥辣鸡点心啊啊啊啊啊啊啊啊啊啊啊</p>
+          <p>规格：4444*777</p>
+          <p>
+              <span>优惠套餐</span>
+              <span>混搭满赠</span>
+              <span>打折</span>
+            </p>
+          <p>
+            <span>￥20.00</span>
+            <img src="../../assets/icon43.png" alt="" class="gocart">
+          </p>
+        </div>
+         <div class="searchItem">
+          <div class="onephoto">
+            <img src="../../assets/icon12.png" alt="" class="photo">
+          </div>
+          
+          <p>墨西哥辣鸡点心啊啊啊啊啊啊啊啊啊啊啊</p>
+          <p>规格：4444*777</p>
+          <p>
+              <span>优惠套餐</span>
+              <span>混搭满赠</span>
+              <span>打折</span>
+            </p>
+          <p>
+            <span>￥20.00</span>
+            <span>奖</span>
+            <span>￥8.00</span>
+            <img src="../../assets/icon43.png" alt="" class="gocart">
+          </p>
+        </div>       
+   </div>
+    <div class="over">
+        <!-- <p>已经到底啦</p> -->
+        <img src="../../assets/icon43.png" alt="">
+        <img src="../../assets/icon54.png" alt="">
+    </div>
+    </div>
+  
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default({
+    name:"prodsList",
+    data() {
+        return{
+
+        }
+    },
+    props:["listStatus"],
+    methods:{
+     
+    }
+  })
+</script>
+
+<style>
+
+.proList{
+   margin-top:208px;
+   margin-bottom:180px;
+}
+/*content横着布局方式*/
+.content ,.changeItem{
+  width:100%;
+  overflow: hidden;
+  position: absolute;
+  top:208px;
+  bottom:180px;
+  overflow-y:scroll;
+  -webkit-overflow-scrolling: touch;
+  background:#ebecf0;
+}
+.content .searchItem:nth-child(even){
+  float:right;
+  width:367px;
+  height:594px;
+  margin-top:14px;
+  background: #fff;
+}
+.content .searchItem:nth-child(odd){
+  float:left;
+  width:367px;
+  height:594px;
+  margin-top:14px;
+  background: #fff;
+}
+.content .searchItem .onephoto{
+  width:367px;
+  height:367px;
+  position: relative;
+}
+.content .searchItem .onephoto .photo{
+  width:367px;
+  height:367px;
+}
+
+.content .searchItem>p:nth-child(2){
+  width:330px;
+  height:80px;
+  font-size: 30px;
+  color: #3B456C;
+  letter-spacing: 0;
+  line-height: 40px;
+  margin:19px 14px 0 23px;
+}
+.content .searchItem>p:nth-child(3){
+  font-size: 22px;
+  color: #9DA2B5;
+  letter-spacing: 0;
+  height:30px;
+  margin:16px 0 10px 23px;
+}
+.content .searchItem>p:nth-child(4){
+  /*width:367px;*/
+  height:48px;
+  padding-top:10px;
+  opacity: 0.9;
+  background: #FFFFFF;
+  position: absolute;
+  margin-top:-210px;
+  margin-left:10px;
+
+}
+.content .searchItem>p:nth-child(4) span{
+  font-size: 18px;
+  color: #FF783C;
+  letter-spacing: 0;
+  border: 1px solid #FF783C;
+  border-radius: 2px;
+  display:inline-block;
+  padding:1px 6px 1px 6px;
+}
+.content .searchItem>p:nth-child(5){
+  margin-top:20px;
+}
+.content .searchItem>p:nth-child(5) span{
+  font-size: 30px;
+  color: #FF783C;
+  letter-spacing: 0;
+  margin:16px 0 0 15px;
+}
+.content .searchItem .gocart{
+  width:48px;
+  height:48px;
+  float:right;
+  margin-right:24px;
+}
+
+/*changeItem竖着布局方式*/
+.changeItem .searchItem{
+  height:198px;
+  width:100%;
+  border-top:2px solid #F1F2F7;
+  background:#fff;
+  padding:50px 40px 50px 32px;
+
+}
+.changeItem .searchItem .onephoto{
+}
+.changeItem .searchItem .onephoto .photo{
+  width:200px;
+  height:200px;
+  float:left;
+
+}
+/**/
+.changeItem .searchItem>p:nth-child(2){
+  width:450px;
+  height:80px;
+  float:left;
+  font-size: 30px;
+  color: #3B456C;
+  letter-spacing: 0;
+  line-height: 40px;
+  margin-left:28px;
+
+}
+.changeItem .searchItem>p:nth-child(3){
+  font-size: 22px;
+  color: #9DA2B5;
+  letter-spacing: 0;
+  float:left;
+  width:450px;
+  margin-left:28px;
+}
+.changeItem .searchItem>p:nth-child(4){
+  width:450px;
+  height:40px;
+  float:left;
+  margin-top:8px;
+  margin-left:28px;
+}
+.changeItem .searchItem>p:nth-child(4) span{
+  float:left;
+  font-size: 18px;
+  color: #FF783C;
+  letter-spacing: 0;
+  border: 1px solid #FF783C;
+  border-radius: 2px;
+  display:inline-block;
+  padding:1px 10px 1px 10px;
+  margin-right:10px;
+}
+.changeItem .searchItem>p:nth-child(5){
+  margin-top:4px;
+  height:44px;
+  line-height:44px;
+  width:450px;
+  float:left;
+  margin-left:28px;
+}
+.changeItem .searchItem>p:nth-child(5) span:nth-child(1){
+  font-size: 30px;
+  color: #FF783C;
+  letter-spacing: 0;
+}
+.changeItem .searchItem>p:nth-child(5) span:nth-child(2){
+  font-size: 18px;
+  color: #FFFFFF;
+  letter-spacing: 0;
+  background: #FF7A3F;
+  border-radius: 4px;
+  margin:0 10px 0 40px;
+  width:30px;
+  height:30px;
+  text-align: center;
+
+}
+.changeItem .searchItem>p:nth-child(5) span:nth-child(3){
+  font-size: 22px;
+  color: #FF783C;
+  letter-spacing: 0;
+
+}
+.changeItem .searchItem .gocart{
+  width:48px;
+  height:48px;
+  float:right;
+}
+.over{
+  width:100%;
+  padding:30px 0;
+  background:#ebecf0;
+  text-align: center;
+  overflow:hidden;
+  position: fixed;
+  bottom:0;
+}
+.over p{
+  margin :0 20px;
+  display:inline-block;
+  background:url(../../assets/icon8.png)  no-repeat center;
+  width:400px;
+  height:40px;
+  background-size:100%;
+}
+.over img {
+  display:inline-block;
+  width:112px;
+  height:112px;
+  float:right;
+  margin:0 20px 15px 0;
+}
+</style>
--- src/component/listcomponent.vue
+++ src/component/listcomponent.vue
@@ -1,123 +1,91 @@
 <template>
-  <div>
+  <div id="prods">
     <header>
-      <div><img src="1.jpg" alt=""/></div>
+      <div><img src="../assets/icon10.png" alt=""/></div>
       <input type="text" placeholder="请输入关键字搜索商品">
-      <div><img src="1.jpg" alt=""/></div>
+      <div @click="changeList" :class="{'statusactive':status,'statusleft':!status}"></div>
     </header>
     <div class="border"></div>
     <div class="selectList">
       <ul>
-        <li><span>综合</span></li>
-        <li><span>销量</span></li>
-        <li><span>价格</span><img src="1.jpg" alt=""></li>
-        <li><span>筛选</span><img src="1.jpg" alt=""></li>
+        <li>综合</li>
+        <li>销量</li>
+        <li @click="changePrice" :class="{'lowprice':price,'topprice':!price}">价格</li>
+        <li>筛选</li>
       </ul>
     </div>
-   <div class="content">
-        <div class="searchItem">
-          <div class="onephoto">
-            <img src="1.jpg" alt="" class="photo">
-            <p>
-              <span>优惠套餐</span>
-              <span>混搭满赠</span>
-              <span>打折</span>
-            </p>
-          </div>
-          
-          <p>墨西哥辣鸡点心啊啊啊啊啊啊啊啊啊啊啊</p>
-          <p>规格：4444*777</p>
-          <p>
-            <span>￥20.00</span>
-            <img src="a.jpg" alt="" class="gocart">
-          </p>
-        </div>
-         <div class="searchItem">
-          <div class="onephoto">
-            <img src="1.jpg" alt="" class="photo">
-            <p>
-              <span>优惠套餐</span>
-              <span>混搭满赠</span>
-              <span>打折</span>
-            </p>
-          </div>
-          
-          <p>墨西哥辣鸡点心啊啊啊啊啊啊啊啊啊啊啊</p>
-          <p>规格：4444*777</p>
-          <p>
-            <span>￥20.00</span>
-            <img src="a.jpg" alt="" class="gocart">
-          </p>
-        </div>
-         <div class="searchItem">
-          <div class="onephoto">
-            <img src="1.jpg" alt="" class="photo">
-            <p>
-              <span>优惠套餐</span>
-              <span>混搭满赠</span>
-              <span>打折</span>
-            </p>
-          </div>
-          
-          <p>墨西哥辣鸡点心啊啊啊啊啊啊啊啊啊啊啊</p>
-          <p>规格：4444*777</p>
-          <p>
-            <span>￥20.00</span>
-            <img src="a.jpg" alt="" class="gocart">
-          </p>
-        </div>
-         <div class="searchItem">
-          <div class="onephoto">
-            <img src="1.jpg" alt="" class="photo">
-            <p>
-              <span>优惠套餐</span>
-              <span>混搭满赠</span>
-              <span>打折</span>
-            </p>
-          </div>
-          
-          <p>1墨西哥辣鸡点心啊啊啊啊啊啊啊啊啊啊啊</p>
-          <p>规格：4444*777</p>
-          <p>
-            <span>￥20.00</span>
-            <img src="a.jpg" alt="" class="gocart">
-          </p>
-        </div>
-     
-   </div>
-    <div class="over">
-        <img src="1.jpg" alt="">
-        <span>已经到底啦</span>
-        <img src="1.jpg" alt="">
-     </div>
+    <proLists :listStatus="listStatus"></proLists>
   </div>
 </template>
 
 <script type="text/javascript">
-
+  import proLists from "./childcomponent/prodlistcomponent.vue"
+  export default({
+    name:"prods",
+    components: {
+      proLists
+    },
+    data() {
+      return{
+        status:false,
+        listStatus:false,
+        price:true
+      }
+    },
+    methods:{
+      changeList:function(){
+        this.status=!this.status;
+        this.listStatus=!this.listStatus
+      },
+      changePrice:function(){
+        this.price=!this.price;
+      }
+    }
+  })
 </script>
 
 <style>
 header{
   height:88px;
   line-height:88px;
+  position: fixed;
+  top:0;
+  background:#fff;
+}
+header .statusactive{
+  width:88px;
+  height:88px;
+  margin-left:20px;
+  vertical-align: middle;
+  background:url(../assets/icon46.png) no-repeat center;
+  background-size:70%;
+  float:left;
+
+}
+header .statusleft{
+  float:left;
+  margin-left:20px;
+  width:88px;
+  height:88px;
+  vertical-align: middle;
+  background:url(../assets/icon47.png) no-repeat center;
+  background-size:70%;
 }
 header input{
   width:448px;
   height:62px;
-  background: #EBECF0;
   border-radius: 2px;
   border:none;
   float:left;
   margin-top:13px;
-  font-family: PingFangSC-Regular;
   font-size: 26px;
   color: #9DA2B5;
   letter-spacing: 0;
   padding-left:76px;
-  /*background:url() no-repeat center;*/
+  background: #EBECF0 url(../assets/icon20.png) no-repeat 22px center;
+  background-size:26px 26px;
 }
-header div:nth-child(1),header div:nth-child(3){
+header div:nth-child(1){
   float:left;
   width:113px;
   line-height:88px;
@@ -126,164 +94,69 @@ header div:nth-child(1),header div:nth-child(3){
 }
 
 header div:nth-child(1) img{
-  width:17.6px;
+  width:33.6px;
   height:33.6px;
   vertical-align: middle;
 
 }
-header div:nth-child(3) img{
-  width:42px;
-  height:42px;
-  vertical-align: middle;
-}
-/*header img:nth-child(1){
-  width:17.6px;
-  height:33.6px;
-  margin:0 48px;
-  vertical-align: middle;
-}*/
 .border{
   height:24px;
   background:#EBECF0;
+  position: fixed;
+  top:88px;
+  left:0;
+  overflow: hidden;
+  width:100%;
+}
+.selectList {
+  position: fixed;
+  top:110px;
+
 }
 .selectList ul{
   width:630px;
   height:37px;
   background:#fff;
-  padding:30px 48px 29px 72px;
+  padding:30px 48px 29px 72px;  
+  text-align: left;
+
 }
 .selectList ul li{
   float:left;
-  font-family: Helvetica;
   font-size: 26px;
   color: #343657;
   letter-spacing: 0;
-  margin-right:116px;
+  margin-right:105px;
+}
+.selectList ul li:nth-last-child(){
+  margin-right:0;
+
 }
 .selectList ul li span{
   display:inline-block;
-  width:52px;
+  font-size: 26px;
   height:37px;
   vertical-align: middle;
 
 }
 .selectList ul li:nth-child(4){
   margin-right:0;
+}
+.selectList ul .topprice{
+  width:100px;
+  background: url(../assets/icon0.png) no-repeat right;
+  background-size:30%;
+}
+.selectList ul .lowprice{
+  width:100px;
+  background: url(../assets/icon6.png) no-repeat right;
+  background-size:30%;
+}
+.selectList ul li:nth-child(4){
+  width:100px;
+  background: url(../assets/icon19.png) no-repeat right;
+  background-size:30%;
+}
 
-}
-.selectList ul li:nth-child(3) img{
-  width:11px;
-  height:26px;
-  margin-left:17px;
-  vertical-align: middle;
-}
-.selectList ul li:nth-child(4) img{
-  width:22px;
-  height:26px;
-  margin-left:17px;
-  vertical-align: middle;
-  /*display:inline-block;*/
 
-}
-.content{
-  width:100%;
-  background:#ebecf0;
-  overflow: hidden;
-}
-.content .searchItem:nth-child(even){
-  float:right;
-  width:367px;
-  height:594px;
-  margin-top:14px;
-  background: #fff;
-}
-.content .searchItem:nth-child(odd){
-  float:left;
-  width:367px;
-  height:594px;
-  margin-top:14px;
-  background: #fff;
-}
-.content .searchItem .onephoto{
-  width:367px;
-  height:367px;
-  position: relative;
-  /*background: #fff;*/
-}
-.content .searchItem .onephoto .photo{
-  width:367px;
-  height:367px;
-}
-.content .searchItem .onephoto p{
-  width:367px;
-  height:48px;
-  padding-top:10px;
-  /*line-height:48px;*/
-  opacity: 0.9;
-  background: #FFFFFF;
-  position: absolute;
-  bottom:0;
-  left:0;
-
-}
-.content .searchItem .onephoto p span{
-  font-family: PingFangSC-Regular;
-  font-size: 18px;
-  color: #FF783C;
-  letter-spacing: 0;
-  border: 1px solid #FF783C;
-  border-radius: 2px;
-  display:inline-block;
-  padding:1px 10px 1px 10px;
-  margin-left:8px;
-}
-.content .searchItem>p:nth-child(2){
-  width:330px;
-  height:80px;
-  font-family: PingFangSC-Medium;
-  font-size: 30px;
-  color: #3B456C;
-  letter-spacing: 0;
-  line-height: 40px;
-  margin:19px 14px 0 23px;
-}
-.content .searchItem>p:nth-child(3){
-  font-family: PingFangSC-Regular;
-  font-size: 22px;
-  color: #9DA2B5;
-  letter-spacing: 0;
-  /*width:138px;*/
-  height:30px;
-  margin:16px 0 10px 23px;
-}
-.content .searchItem>p:nth-child(4){
-  margin-top:20px;
-}
-.content .searchItem>p:nth-child(4) span{
-  font-family: PingFangSC-Regular;
-  font-size: 30px;
-  color: #FF783C;
-  letter-spacing: 0;
-  margin:16px 0 0 15px;
-}
-.content .searchItem .gocart{
-  width:48px;
-  height:48px;
-  float:right;
-  margin-right:24px;
-}
-.over{
-  height:50px;
-  width:100%;
-  padding:30px 0;
-  background:#ebecf0;
-  text-align: center;
-
-}
-.over span{
-  margin :0 20px;
-}
-.over img {
-  vertical-align:middle;
-}
 </style>
