e33ad640be092fe1568e68266c618da79cf29f10
zhanggl <zhanggl@11wlw.com>
2017-05-05 14:18:40 +0800

    库户列表

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import secondcomponent from '../component/secondcomponent.vue'
 import clientServer from '../component/clientServer.vue'
 import  First from '../component/firstcomponent.vue'
+import  customer from '../component/customermanagement.vue'
+
 
 export default{
 	routes: [
@@ -15,6 +17,9 @@ export default{
     {
       path: '/clientServer',
       component: clientServer
+    },{
+      path: '/customer',//客户列表
+      component: customer
     }
   ]
 }
