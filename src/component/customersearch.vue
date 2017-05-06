aaa734bb1d2863ff2abdb1180e8458b1ae1e83b8
zhanggl <zhanggl@11wlw.com>
2017-05-06 15:51:26 +0800

    客户搜索

--- src/component/customersearch.vue
+++ src/component/customersearch.vue
@@ -1,23 +1,69 @@
 <template>
 	<div class="heards">
 		<span><img src="../assets/icon10.png"></span>
-		<form>
+		<form  @submit.prevent="submit">
 			<div class="input-wrap">
-				<div><input type="text" name="" value="123132123"><img src="../assets/icon18.png"></div>
+				<div><input type="text" v-model="newTodo"><img v-on:click="removeTodo" src="../assets/icon18.png"></div>
 				
 			</div>
 		</form>
 	</div>
 </template>
 <script type="text/javascript">
+ import Request from  "../util/API";
+ export default {
+	  data () {
+	    return {
+	    	newTodo:'测试测试测试',
+	    }
+},
+	    methods:{
+	    removeTodo:function () {
+	    	this.newTodo=''
+	    },
+	    function () {
+        let pargrm = {
+          pagination:JSON.stringify(this.page),
+          sign: "",
+   		  secret: "",
+          oper:'"getShopList",',
+          type:'"wqCustomer",',
+          version:'10',
+          para:'{"latitude": "30.32765","longitude": "120.17237", "keywords": "139107", "picno": "355328","type": 0}'
+        }
+        },
+	    submit:function () {
+	    	//ajax调用
+        Request.post(pargrm).then(function (res) {
+          console.log(res)
+          Indicator.close();
+        }).catch(function (error) {
+          Indicator.close();
+          if (error.response) {
+            // 请求已发出，但服务器响应的状态码不在 2xx 范围内
+            console.log(error.response.status);
+            Toast({
+              message: error.response.status,
+              duration: 2000
+            });
+          } else {
+            console.log('Error', error.message);
+            Toast({
+              message: error.message,
+              duration: 2000
+            });
+          }
+        })
+	    }
+	}
+}
 </script>
 <style scoped>
 .heards{width: 100%;height: 84px;background-color: #fff;}
 .heards span img{height: 33.6px; float: left;padding: 28px 0 27.4px 37px;}
 .heards .input-wrap{border:0;float: left;}
-.heards .input-wrap div{float: left;}
-.heards .input-wrap input{border:none;background-color:#EBECF0;width: 548px;height: 62px;margin-top: 11px;margin-left: 41.4px;font-size: 30px;
-color: #3B456C;}
-.heards .input-wrap div{position: relative;}
-.heards .input-wrap img{width: 20.4px;position: absolute;right: 20px;top: 32px}
+.heards .input-wrap div{}
+.heards .input-wrap input{border:none;background-color:#EBECF0;width: 490px;height: 62px;margin-left: 24px;font-size: 30px;color: #3B456C;line-height: 62px}
+.heards .input-wrap div{float: left;width: 548px; height: 62px;background-color:#EBECF0;margin-top: 11px;margin-left: 41.4px}
+.heards .input-wrap img{width: 20.4px;line-height: 62px}
 </style>
