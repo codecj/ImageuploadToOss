555644d355cfbb37aabf49bbbb730b8c6f35f9a9
zhanggl <zhanggl@11wlw.com>
2017-05-10 14:40:55 +0800

    搜索聚焦

--- src/views/customersearch.vue
+++ src/views/customersearch.vue
@@ -31,6 +31,14 @@
 		loading: require('../assets/holde.png'),
 		listenEvents: ['scroll']
 	})
+    // 注册一个全局自定义指令 v-focus
+Vue.directive('focus', {
+  // 当绑定元素插入到 DOM 中。
+  inserted: function (el) {
+    // 聚焦元素
+    el.focus()
+  }
+})
 	export default {
 		data() {
 			return {
