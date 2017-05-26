3a72311cd10c9b7694f6441a8389be09b6665793
cuijing <cuijing@11wlw.com>
2017-05-26 19:18:16 +0800

    a

--- src/components/oneProd.vue
+++ src/components/oneProd.vue
@@ -28,7 +28,7 @@
                 <p>
                     <span>￥{{item.NET_PRICE}}</span>
                     <span v-show="item.COMMISSION_PRICE">奖</span>
-                    <span v-show="item.COMMISSION_PRICE">"￥" + item.COMMISSION_PRICE</span>
+                    <span v-show="item.COMMISSION_PRICE">{{"￥" + item.COMMISSION_PRICE}}</span>
                     <img src="../assets/icon57.png" alt="" class="gocart" v-if="item.ATP_QTY==0">
                     <img src="../assets/icon2.png" alt="" class="gocart" @click.stop="gocart(item.STK_C)" v-else="item.ATP_QTY>0">
                 </p>
