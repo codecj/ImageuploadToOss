47c8f88c72afcd951879e1c83af767b7cc95e06b
liudunhui <liudunhui@11wlw.com>
2017-07-01 18:02:03 +0800

    换提示语

--- src/views/carSale/CarSellerManagerHome.vue
+++ src/views/carSale/CarSellerManagerHome.vue
@@ -156,7 +156,7 @@ export default {
                 if (parseInt(resData.code) != 200) {
                     if ((JSON.parse(response.data.result).code) == "4") {
                         Toast({
-                            message: "暂无商品",
+                            message: "商品列表暂无数据",
                             duration: 2000
                         });
                         return;
