e08d77d4e5ff9a2d80e3c2d1108e65f8c8f6d0e6
mxf <mxf@11wlw.com>
2017-08-18 15:16:13 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -71,7 +71,7 @@ module.exports = {
     alias: {vue: 'vue/dist/vue.js'}
   },
   devServer: {
-    host:'192.168.200.251',
+    // host:'192.168.200.251',
     historyApiFallback: true,
     noInfo: true
   },
@@ -100,4 +100,29 @@ if (process.env.NODE_ENV === 'production') {
       minimize: true
     })
   ])
+}else{
+  module.exports.output = {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: '[name].js'
+  }
+  module.exports.devServer = {
+    host:'192.168.200.251',
+    historyApiFallback: true,
+    noInfo: true
+  }
+  module.exports.plugins = [
+    new webpack.LoaderOptionsPlugin({
+      vue: {
+        // use custom postcss plugins
+      postcss: [require('postcss-px2rem')({
+        baseDpr: 2,             // base device pixel ratio (default: 2)
+        threeVersion: false,    // whether to generate @1x, @2x and @3x version (default: false)
+        remVersion: true,       // whether to generate rem version (default: true)
+        remUnit: 75,            // rem unit value (default: 75)
+        remPrecision: 6         // rem precision (default: 6)
+      })]
+      }
+    })
+  ]
 }
