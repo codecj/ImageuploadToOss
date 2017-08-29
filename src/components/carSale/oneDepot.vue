34d6f301e9e6bb626bda049a85ede7f1df97a033
cuijing <cuijing@11wlw.com>
2017-08-29 18:06:12 +0800

    d

--- src/components/carSale/oneDepot.vue
+++ src/components/carSale/oneDepot.vue
@@ -5,7 +5,6 @@
 	    <div><img v-lazy="item.URL_ADDR" alt=""></div>
 	    <div>
 	    	<p>{{item.STK_NAME}}</p>
-	    	<p>123123232</p>
 	    	<p><span>已添回库数</span><span>{{item.OLDQTY}}</span>	</p>
 	    	<p><span>车上库存</span><span>{{item.STOCK}}</span></p>
 	    	<p><span @click="backDepot(item)">修改</span></p>
@@ -89,6 +88,7 @@
 	width:100%;
 	height:80px;
 }
+
 .depot div:nth-child(3) p:nth-child(2){
 	color: #3B456C;
 	letter-spacing: 0;
@@ -96,33 +96,26 @@
 	height:30px;
 	margin-bottom:10px;
 }
-.depot div:nth-child(3) p:nth-child(3){
-	color: #3B456C;
-	letter-spacing: 0;
-	width:100%;
-	height:30px;
-	margin-bottom:10px;
-}
-.depot div:nth-child(3) p:nth-child(3) span:nth-child(1),
-.depot div:nth-child(3) p:nth-child(4) span:nth-child(1)
+.depot div:nth-child(3) p:nth-child(2) span:nth-child(1),
+.depot div:nth-child(3) p:nth-child(3) span:nth-child(1)
 {
 	color:#9DA2B5;
 	font-size:28px;
 	display:inline-block;
 	margin-right:20px;
 }
-.depot div:nth-child(3) p:nth-child(3) span:nth-child(2),
-.depot div:nth-child(3) p:nth-child(4) span:nth-child(1){
+.depot div:nth-child(3) p:nth-child(2) span:nth-child(2),
+.depot div:nth-child(3) p:nth-child(3) span:nth-child(1){
 	font-size:28px;
 }
-.depot div:nth-child(3) p:nth-child(5){
+.depot div:nth-child(3) p:nth-child(4){
 	height:70px;
 	line-height: 70px;
 	position: absolute;
-	bottom:-40px;
+	bottom:-50px;
 	right:0;
 }
-.depot div:nth-child(3) p:nth-child(5) span{
+.depot div:nth-child(3) p:nth-child(4) span{
 	width:120px;
 	height:70px;
 	text-align: right;
